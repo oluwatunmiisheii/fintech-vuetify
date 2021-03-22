de19af0207ce3b90fac7935fafebc3d166afbe9e
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-03-22 16:35:07 +0100

    append token to axios header in main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { apiClient } from "@/services/axios";
 
 Vue.config.productionTip = false
+
+// conditional logic to check if user there is a valid token
+if (localStorage.getItem('token')) {
+  let token = localStorage.getItem("token");
+  apiClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
 
 new Vue({
   router,
