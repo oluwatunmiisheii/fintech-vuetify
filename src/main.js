b5b3fee527d6959ff955a862727ceb580448a527
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-03-23 05:25:25 +0100

    chore: imported and registered vue izitoast

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import router from './router'
 import store from './store'
 import { apiClient } from "@/services/axios";
 import vuetify from './plugins/vuetify';
+import VueIziToast from "vue-izitoast";
+import "izitoast/dist/css/iziToast.css";
+
+Vue.use(VueIziToast);
 
 Vue.config.productionTip = false
+
+// set application base url
+apiClient.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 
 // conditional logic to check if user there is a valid token
 if (localStorage.getItem('token')) {
