3f705d09f881464ade903edae5ce96ae5532f0fd
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-03-23 09:37:52 +0100

    feat: payment history

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,12 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import auth from "./modules/auth";
-
+import paymentHistory from "./modules/paymentHistory";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    paymentHistory
   }
 });
